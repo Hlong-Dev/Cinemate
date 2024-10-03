abbf1c805d5ea59e14405c72bc2746e0d31657f4
hlongday <waity7colors@gmail.com>
2024-10-03 22:23:16 +0700

    add jwt handle

--- src/ChatRoom.js
+++ src/ChatRoom.js
@@ -1,149 +1,293 @@
-﻿import React, { useEffect, useState } from 'react';
+﻿import React, { useEffect, useState, useRef } from 'react';
 import SockJS from 'sockjs-client';
-import { Stomp } from '@stomp/stompjs'; // Sử dụng @stomp/stompjs
+import { Client } from '@stomp/stompjs';
 import { useParams } from 'react-router-dom';
-import './ChatRoom.css'; // Import file CSS
+import './ChatRoom.css';
 import { getUserFromToken } from './utils/jwtUtils';
 import ReactPlayer from 'react-player';
+import Compressor from 'compressorjs';
 
 const ChatRoom = () => {
-    const { roomId } = useParams(); // Lấy roomId từ URL
+    const { roomId } = useParams();
     const [messages, setMessages] = useState([]);
     const [stompClient, setStompClient] = useState(null);
     const [messageContent, setMessageContent] = useState('');
-    const [connected, setConnected] = useState(false); // Trạng thái kết nối WebSocket
-    const currentUser = getUserFromToken()?.username || 'Unknown'; // Lấy tên người dùng từ JWT
-
+    const [selectedImage, setSelectedImage] = useState(null); // State to store selected image
+    const [connected, setConnected] = useState(false);
+    const currentUser = getUserFromToken()?.username || 'Unknown';
+    const chatMessagesRef = useRef(null); // Reference to the chat messages container
+    const inputRef = useRef(null); // Reference to the input field
 
     useEffect(() => {
-        // Kết nối WebSocket tới Spring Boot
-        const socket = new SockJS('http://localhost:8080/ws'); // Kết nối tới endpoint WebSocket
-        const stompClient = Stomp.over(socket);
+        const socket = new SockJS('http://localhost:8080/ws');
+        const client = new Client({
+            webSocketFactory: () => socket,
+            reconnectDelay: 5000, // Attempt to reconnect every 5 seconds
+            heartbeatIncoming: 10000,
+            heartbeatOutgoing: 10000,
+            onConnect: () => {
+                setConnected(true);
 
-        stompClient.connect({}, () => {
-            setConnected(true); // Đánh dấu kết nối hoàn tất
-            stompClient.subscribe(`/topic/${roomId}`, (message) => {
-                setMessages(prevMessages => [...prevMessages, JSON.parse(message.body)]);
-            });
-        }, (error) => {
-            console.error('STOMP connection error:', error);
-            setConnected(false); // Đánh dấu kết nối thất bại
+                // Send a join message when connected
+                const joinMessage = {
+                    sender: currentUser,
+                    type: 'JOIN'
+                };
+                client.publish({
+                    destination: `/app/chat.addUser/${roomId}`,
+                    body: JSON.stringify(joinMessage),
+                });
+
+                // Subscribe to the chat room to receive messages
+                client.subscribe(`/topic/${roomId}`, (message) => {
+                    const receivedMessage = JSON.parse(message.body);
+                    setMessages(prevMessages => [...prevMessages, receivedMessage]);
+                });
+            },
+            onStompError: (frame) => {
+                console.error('Broker reported error: ' + frame.headers['message']);
+                console.error('Additional details: ' + frame.body);
+                setConnected(false);
+            },
+            onWebSocketClose: () => {
+                console.error('WebSocket connection closed, attempting to reconnect...');
+                setConnected(false);
+            },
+            onWebSocketError: (error) => {
+                console.error('WebSocket error occurred: ', error);
+                setConnected(false);
+            },
         });
-        setStompClient(stompClient);
 
-        // Ngắt kết nối khi component bị hủy
+        client.activate();
+        setStompClient(client);
+
         return () => {
-            if (stompClient) {
-                stompClient.disconnect();
+            if (client) {
+                const leaveMessage = {
+                    sender: currentUser,
+                    type: 'LEAVE'
+                };
+                try {
+                    client.publish({
+                        destination: `/app/chat.removeUser/${roomId}`,
+                        body: JSON.stringify(leaveMessage),
+                    });
+                } catch (error) {
+                    console.error("Không thể gửi tin nhắn leave, STOMP connection không sẵn sàng.", error);
+                }
+
+                client.deactivate();
             }
         };
-    }, [roomId]);
+    }, [roomId, currentUser]);
+
+
+    // Automatically scroll to the bottom when messages are updated
+    useEffect(() => {
+        if (chatMessagesRef.current) {
+            chatMessagesRef.current.scrollTop = chatMessagesRef.current.scrollHeight;
+        }
+    }, [messages]);
 
     const sendMessage = () => {
-        if (stompClient && connected && messageContent.trim()) {
+        // Check if the STOMP client is connected
+        if (stompClient && stompClient.connected && (messageContent.trim() || selectedImage)) {
             const chatMessage = {
-                sender: currentUser, // Sử dụng tên người dùng từ JWT
-                content: messageContent,
+                sender: currentUser,
+                content: messageContent || '',
+                image: null, // No image yet
                 type: "CHAT"
             };
-            stompClient.send(`/app/chat.sendMessage/${roomId}`, {}, JSON.stringify(chatMessage));
-            setMessageContent(''); // Xóa nội dung tin nhắn sau khi gửi
+
+            if (selectedImage) {
+                new Compressor(selectedImage, {
+                    quality: 0.3,
+                    maxWidth: 800,
+                    maxHeight: 800,
+                    mimeType: 'image/jpeg',
+                    success(result) {
+                        const reader = new FileReader();
+                        reader.onloadend = () => {
+                            const base64Image = reader.result;
+                            const base64Size = base64Image.length * (3 / 4) / 1024;
+
+                            if (base64Size <= 64) {
+                                chatMessage.image = base64Image.split(',')[1];
+                                chatMessage.type = "IMAGE";
+                            } else {
+                                console.error('Kích thước ảnh quá lớn, hãy thử nén thêm.');
+                            }
+
+                            try {
+                                stompClient.publish({
+                                    destination: `/app/chat.sendMessage/${roomId}`,
+                                    body: JSON.stringify(chatMessage)
+                                });
+                            } catch (error) {
+                                console.error("Không thể gửi tin nhắn, STOMP connection không sẵn sàng.", error);
+                            }
+
+                            setMessageContent('');
+                            setSelectedImage(null); // Clear the selected image after sending
+                        };
+                        reader.readAsDataURL(result);
+                    },
+                    error(err) {
+                        console.error('Compression error:', err);
+                    },
+                });
+            } else {
+                try {
+                    stompClient.publish({
+                        destination: `/app/chat.sendMessage/${roomId}`,
+                        body: JSON.stringify(chatMessage)
+                    });
+                    setMessageContent('');
+                } catch (error) {
+                    console.error("Không thể gửi tin nhắn, STOMP connection không sẵn sàng.", error);
+                }
+            }
         } else {
-            console.error("Chưa kết nối WebSocket hoặc tin nhắn trống.");
+            console.error("WebSocket chưa kết nối hoặc tin nhắn trống.");
+        }
+    };
+
+
+    const handleImageUpload = (e) => {
+        if (e.target.files[0]) {
+            setSelectedImage(e.target.files[0]); // Set the selected image
+            inputRef.current.focus(); // Focus back to input after selecting an image
         }
     };
 
     return (
         <div className="container">
-            <div class="headerr">
-                <div class="top-bar">
-                    <i class="fas fa-times icon-left"></i> 
-                    <i class="fas fa-bars icon-left"></i> 
-                    <i class="fas fa-cog icon-center"></i>
-                    <i class="fas fa-search icon-center"></i>
-                    <div class="logo">
+            <div className="headerr">
+                <div className="top-bar">
+                    <i className="fas fa-times icon-left"></i>
+                    <i className="fas fa-bars icon-left"></i>
+                    <i className="fas fa-cog icon-center"></i>
+                    <i className="fas fa-search icon-center"></i>
+                    <div className="logo">
                         <img src="https://i.imgur.com/Rp89NPj.png" alt="Rave" />
                     </div>
-                    <i class="fas fa-check icon-right"></i> 
-                    <i class="fas fa-user-friends icon-right"></i> 
+                    <i className="fas fa-check icon-right"></i>
+                    <i className="fas fa-user-friends icon-right"></i>
                 </div>
             </div>
 
-
-            {/* Phần video */}
             <div className="main-content">
                 <div className="video-section">
-                    {/* Thêm phần phát video tại đây */}
                     <ReactPlayer
-                        url="https://localhost/video/play" // URL video từ Caddy server
+                        url="https://localhost/video/play"
                         className="react-player"
-                        playing={true} // Tự động phát video
-                        controls={true} // Hiển thị điều khiển video
-                        width="100%" // Chiều rộng của video
-                        height="100%" // Chiều cao của video
+                        playing={true}
+                        controls={true}
+                        width="100%"
+                        height="100%"
                     />
                 </div>
-                {/* Phần chat */}
                 <div className="chat-section">
-                    <div className="chat-messages" id="chatMessages">
+                    <div className="chat-messages" id="chatMessages" ref={chatMessagesRef}>
                         <ul>
-                            {messages.map((message, index) => (
-                                <li key={index} className={message.sender === currentUser ? "message-item sent" : "message-item received"}>
-                                    <div className={message.sender === currentUser ? "message-container sent-container" : "message-container received-container"}>
-                                        {/* Avatar and Message content */}
-                                        {message.sender === currentUser ? (
-                                            <>
-                                                <div className="message-content">
-                                                    <div className="message-text">
-                                                        <strong></strong>{message.content}
+                            {messages.map((message, index) => {
+                                const previousMessageSender = index > 0 ? messages[index - 1].sender : null;
+                                const isSameSenderAsPrevious = message.sender === previousMessageSender;
+
+                                return (
+                                    <li key={index} className={message.type === 'JOIN' || message.type === 'LEAVE' ? "message-item system-notification" : message.sender === currentUser ? "message-item sent" : "message-item received"}>
+                                        <div className={message.type === 'JOIN' || message.type === 'LEAVE' ? "system-message-container" : message.sender === currentUser ? "message-container sent-container" : "message-container received-container"}>
+
+                                            {/* Only show avatar and username if this message is from a new sender */}
+                                            {!isSameSenderAsPrevious && (
+                                                <>
+                                                    <div className="message-avatar">
+                                                        <img src="https://i.imgur.com/Tr9qnkI.jpeg" alt="Avatar" />
                                                     </div>
-                                                </div>
-                                                <div className="message-avatar">
-                                                    <img src="https://i.imgur.com/uG5mEvD.png" alt="Avatar" />
-                                                </div>
-                                            </>
-                                        ) : (
-                                            <>
-                                                <div className="message-avatar">
-                                                    <img src="https://i.imgur.com/uG5mEvD.png" alt="Avatar" />
-                                                </div>
-                                                <div className="message-content">
-                                                    <div className="message-text">
-                                                        <strong>{message.sender}: </strong>{message.content}
+                                                    <div className="message-username">
+                                                        <strong>{message.sender}:</strong>
                                                     </div>
+                                                </>
+                                            )}
+
+                                            {message.type === 'JOIN' ? (
+                                                <div className="system-message">
+                                                    <em>{message.sender} has joined the chat</em>
                                                 </div>
-                                            </>
-                                        )}
-                                    </div>
-                                </li>
-                            ))}
+                                            ) : message.type === 'LEAVE' ? (
+                                                <div className="system-message">
+                                                    <em>{message.sender} has left the chat</em>
+                                                </div>
+                                            ) : (
+                                                <>
+                                                    {message.type === 'IMAGE' && message.image && (
+                                                        <div className="message-image">
+                                                            <img src={`data:image/png;base64,${message.image}`} alt="Sent Image" style={{ maxWidth: '200px', marginTop: '10px' }} />
+                                                        </div>
+                                                    )}
+                                                    {message.content && (
+                                                        <div className="message-content">
+                                                            <div className="message-text">
+                                                                {isSameSenderAsPrevious ? message.content : null}
+                                                                {!isSameSenderAsPrevious && (
+                                                                    <>
+                                                                        {message.content}
+                                                                    </>
+                                                                )}
+                                                            </div>
+                                                        </div>
+                                                    )}
+                                                </>
+                                            )}
+                                        </div>
+                                    </li>
+                                );
+                            })}
                         </ul>
+
+
+
                     </div>
 
-                    {/* Ô nhập tin nhắn */}
+                    {/* Display image preview above the input */}
+                    {selectedImage && (
+                        <div className="image-preview-container">
+                            <img src={URL.createObjectURL(selectedImage)} alt="Preview" style={{ maxWidth: '200px', marginBottom: '10px' }} />
+                            <button onClick={() => setSelectedImage(null)} style={{ marginLeft: '10px' }}>Remove</button>
+                        </div>
+                    )}
+
                     <div className="chat-input-container">
                         <input
                             className="chat-input"
                             type="text"
                             value={messageContent}
+                            ref={inputRef} // Reference for input
                             onChange={(e) => setMessageContent(e.target.value)}
                             onKeyPress={(e) => {
                                 if (e.key === 'Enter') {
                                     e.preventDefault();
                                     sendMessage();
                                 }
-                            }}  // Sự kiện nhấn Enter để gửi tin nhắn
-                            placeholder="Nhập tin nhắn..."
+                            }}
+                            placeholder="Chat"
                         />
-                        <button onClick={sendMessage} disabled={!connected} className="send-button">
-                            <i className="fas fa-paper-plane"></i>
-                        </button>
+
+                        <label htmlFor="imageUpload" style={{ cursor: 'pointer' }}>
+                            <img src="https://i.imgur.com/CqCdOHG.png" alt="Upload Icon" style={{ width: '30px', height: '30px', marginLeft: '10px' }} />
+                        </label>
+                        <input
+                            id="imageUpload"
+                            type="file"
+                            accept="image/*"
+                            onChange={handleImageUpload} // Handle image upload
+                            style={{ display: 'none' }} // Hide the input, we use the label with the icon for the click event
+                        />
                     </div>
 
                     {!connected && <p className="connection-status">Đang kết nối đến máy chủ chat...</p>}
                 </div>
-
-
             </div>
         </div>
     );
